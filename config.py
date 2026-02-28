fab2d64d4bca94b2d789cbbdafe7a22c66c95370
Kirtan-Patel234 <patel.kirtan2004@outlook.com>
2026-02-28 13:29:08 -0600

    Ignore virtual envs and load config from .env

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 ﻿import os
+from dotenv import load_dotenv
+
+# add API_KEY to .env and load it in for security
+load_dotenv()
+
 
 class Config:
     # OpenAI API Configuration
-    OPENAI_API_KEY = "input_api_key"  
+    OPENAI_API_KEY = os.getenv("OPENAI_KEY")
     OPENAI_MODEL = "gpt-3.5-turbo"
-    
+
     # System Prompt
     SYSTEM_PROMPT = """You are an academic advising assistant for multiple departments at UIUC.
     You have access to official department documents and contact information.
